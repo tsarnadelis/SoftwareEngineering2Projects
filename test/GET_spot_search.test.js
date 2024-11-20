f7f07727cd7fcce79530b22e60d672fa925f07da
tsarnadelis <thanasischa2@gmail.com>
2024-11-20 17:31:56 +0200

    Write unit test for GET /spot/search

--- test/GET_spot_search.test.js
+++ test/GET_spot_search.test.js
@@ -27,7 +27,29 @@ test("searchSpot function returns correct JSON structure", async (t) => {
   	t.is(spot.chargerAvailability, true); // chargerAvailability should be true
 });
 
-test("searchSpot function with empty attributes returns correct JSON structure", async (t) => {
+test("searchSpot function with only address returns correct JSON structure", async (t) => {
+	const body = await searchSpot("address","",); // charger CANNOT BE EMPTY, it is needed and boolean
+  	t.true(Array.isArray(body));  // response should be an array
+  	t.is(body.length, 2);         // response should have 2 elements
+  	spot = body[0];              // check the first element, the second should be the same
+  	t.is(spot.address, "address"); // address should be "address"
+  	t.is(spot.id, 0);              // id should be 0
+  	t.is(spot.type, "type");       // type should be "type"
+  	t.is(spot.chargerAvailability, true); // chargerAvailability should be true
+});
+
+test("searchSpot function with only type returns correct JSON structure", async (t) => {
+	const body = await searchSpot("","type",); // charger CANNOT BE EMPTY, it is needed and boolean
+  	t.true(Array.isArray(body));  // response should be an array
+  	t.is(body.length, 2);         // response should have 2 elements
+  	spot = body[0];              // check the first element, the second should be the same
+  	t.is(spot.address, "address"); // address should be "address"
+  	t.is(spot.id, 0);              // id should be 0
+  	t.is(spot.type, "type");       // type should be "type"
+  	t.is(spot.chargerAvailability, true); // chargerAvailability should be true
+});
+
+test("searchSpot function with only charger returns correct JSON structure", async (t) => {
 	const body = await searchSpot("","",true); // charger CANNOT BE EMPTY, it is needed and boolean
   	t.true(Array.isArray(body));  // response should be an array
   	t.is(body.length, 2);         // response should have 2 elements
@@ -39,13 +61,13 @@ test("searchSpot function with empty attributes returns correct JSON structure",
 });
 
 test("GET /spot/search endpoint returns valid JSON content-type header", async (t) => {
-	const { headers, statusCode } = await t.context.got('spot/search?address="address"&type="type"&charger=true');
+	const { headers, statusCode } = await t.context.got('spot/search?address=address&type=type&charger=true');
 	t.is(statusCode, 200);                   // The status code should be 200
 	t.is(headers["content-type"], "application/json"); // The content-type should be JSON
 });
 
 test("GET /spot/search endpoint returns 200 and JSON", async (t) => {
-	const { body, statusCode } = await t.context.got('spot/search?address="address"&type="type"&charger=true');
+	const { body, statusCode } = await t.context.got('spot/search?address=address&type=type&charger=true');
   	t.is(statusCode, 200);        // status code should be 200
  	t.true(Array.isArray(body));  // response should be an array
   	t.is(body.length, 2);         // response should have 2 elements
@@ -56,8 +78,32 @@ test("GET /spot/search endpoint returns 200 and JSON", async (t) => {
   	t.is(spot.chargerAvailability, true); // chargerAvailability should be true
 });
 
-test("GET /spot/search endpoint handles request with empty attributes with 200", async (t) => {
-	const { body , statusCode } = await t.context.got('spot/search?address=""&type=""&charger=true'); // charger CANNOT BE EMPTY, it is needed and boolean
+test("GET /spot/search endpoint with only charger returns 200", async (t) => {
+	const { body , statusCode } = await t.context.got('spot/search?address=&type=&charger=true'); // charger CANNOT BE EMPTY, it is needed and boolean
+	t.is(statusCode, 200);   // The status code should be 200
+	t.true(Array.isArray(body));  // response should be an array
+  	t.is(body.length, 2);         // response should have 2 elements
+  	spot = body[0];              // check the first element, the second should be the same
+  	t.is(spot.address, "address"); // address should be "address"
+ 	t.is(spot.id, 0);              // id should be 0
+  	t.is(spot.type, "type");       // type should be "type"
+  	t.is(spot.chargerAvailability, true); // chargerAvailability should be true
+});
+
+test("GET /spot/search endpoint with only address returns 200", async (t) => {
+	const { body , statusCode } = await t.context.got('spot/search?address=address&type=&charger=false'); // charger CANNOT BE EMPTY, it is needed and boolean
+	t.is(statusCode, 200);   // The status code should be 200
+	t.true(Array.isArray(body));  // response should be an array
+  	t.is(body.length, 2);         // response should have 2 elements
+  	spot = body[0];              // check the first element, the second should be the same
+  	t.is(spot.address, "address"); // address should be "address"
+ 	t.is(spot.id, 0);              // id should be 0
+  	t.is(spot.type, "type");       // type should be "type"
+  	t.is(spot.chargerAvailability, true); // chargerAvailability should be true
+});
+
+test("GET /spot/search endpoint with only type returns 200", async (t) => {
+	const { body , statusCode } = await t.context.got('spot/search?address=&type=type&charger=false'); // charger CANNOT BE EMPTY, it is needed and boolean
 	t.is(statusCode, 200);   // The status code should be 200
 	t.true(Array.isArray(body));  // response should be an array
   	t.is(body.length, 2);         // response should have 2 elements
@@ -78,17 +124,8 @@ test("GET /spot/search endpoint handles request without attributes with 400", as
 	t.is(error.response.statusCode, 400);   // The status code should be 400 (Bad Request)
 });
 
-test("GET /spot/search endpoint handles request without address attribute with 400", async (t) => {
-	const error = await t.throwsAsync(() => t.context.got('spot/search?type="type"&charger=true')); // Address is missing
-	t.is(error.response.statusCode, 400);   // The status code should be 400 (Bad Request)
-});
-
-test("GET /spot/search endpoint handles request without type attribute with 400", async (t) => {
-	const error = await t.throwsAsync(() => t.context.got('spot/search?address="address"&charger=true')); // Type is missing
-	t.is(error.response.statusCode, 400);   // The status code should be 400 (Bad Request)
-});
-
-test("GET /spot/search endpoint handles request without charger attribute with 400", async (t) => {
-	const error = await t.throwsAsync(() => t.context.got('spot/search?address="address"&type="type"')); // Charger is missing
-	t.is(error.response.statusCode, 400);   // The status code should be 400 (Bad Request)
+test("GET /spot/search with attributes not corresponding to the expected values returns error", async (t) => {
+	const { body, statusCode } = await t.context.got('spot/search?address=&type=&charger=false');
+	t.is(body.message, "No matching parking spots found."); // The message should be "No matching parking spots found."
+	t.is(body.status, 404); // The status should be 404
 });
