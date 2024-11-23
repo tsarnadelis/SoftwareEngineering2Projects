1a6aa82eee74690e603bd65fd969ff6e308451ac
afotiadis <afotiadis@ece.auth.gr>
2024-11-23 19:05:20 +0200

    Update PUT_reservation_id.test.js

--- test/PUT_reservation_id.test.js
+++ test/PUT_reservation_id.test.js
@@ -16,7 +16,7 @@ test.after.always((t) => {
 });
 
 test('PUT /reservation/{id} 200 for succesful modification of a spot', async (t) => {
-  const { body, statusCode } = await t.context.got.put('reservation/1', {
+  const { body, statusCode } = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "2024-11-19",
       "duration": "2024-11-19T11:00:00Z",
@@ -31,7 +31,7 @@ test('PUT /reservation/{id} 200 for succesful modification of a spot', async (t)
 });
 
 test ('PUT /reservation/{id} 400 if query_id !== body_id', async (t) => {
-  const {body} = await t.context.got.put('reservation/1', {
+  const {body} = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "2024-11-19",
       "duration": "2024-11-19T11:00:00Z",
@@ -45,7 +45,7 @@ test ('PUT /reservation/{id} 400 if query_id !== body_id', async (t) => {
 });
 
 test ('PUT /reservation/{id} 400 if query_spotId !== body_spotId', async (t) => {
-  const {body} = await t.context.got.put('reservation/1', {
+  const {body} = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "2024-11-19",
       "duration": "2024-11-19T11:00:00Z",
@@ -59,7 +59,7 @@ test ('PUT /reservation/{id} 400 if query_spotId !== body_spotId', async (t) => 
 });
 
 test ('PUT /reservation/{id} 400 if query_userId !== body_userId', async (t) => {
-  const {body} = await t.context.got.put('reservation/1', {
+  const {body} = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "2024-11-19",
       "duration": "2024-11-19T11:00:00Z",
@@ -73,7 +73,7 @@ test ('PUT /reservation/{id} 400 if query_userId !== body_userId', async (t) => 
 });
 
 test ('PUT /reservation/{id} 400 for invalid id', async (t) => {
-  const {body} = await t.context.got.put('reservation/invalid', {
+  const {body} = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "2024-11-19",
       "duration": "2024-11-19T11:00:00Z",
@@ -87,7 +87,7 @@ test ('PUT /reservation/{id} 400 for invalid id', async (t) => {
 } );
 
 test ('PUT /reservation/{id} 400 for invalid date', async (t) => {
-  const {body} = await t.context.got.put('reservation/1', {
+  const {body} = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "invalid",
       "duration": "2024-11-19T11:00:00Z",
@@ -101,7 +101,7 @@ test ('PUT /reservation/{id} 400 for invalid date', async (t) => {
 });
 
 test ('PUT /reservation/{id} 400 for invalid duration', async (t) => {
-  const {body} = await t.context.got.put('reservation/1', {
+  const {body} = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "2024-11-19",
       "duration": "invalid",
@@ -115,7 +115,7 @@ test ('PUT /reservation/{id} 400 for invalid duration', async (t) => {
 }); 
 
 test ('PUT /reservation/{id} 400 for invalid startTime', async (t) => {
-  const {body} = await t.context.got.put('reservation/1', {
+  const {body} = await t.context.got.put('reservation/1?date=2024-11-19&duration=2024-11-19T11%3A00%3A00Z&startTime=2024-11-19T08%3A00%3A00Z&userId=1&spotId=1&id=1', {
     json: {
       "date": "2024-11-19",
       "duration": "2024-11-19T11:00:00Z",
