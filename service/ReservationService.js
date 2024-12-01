dd3ae9b26630139560cbd3bb0243cd2bfb215dd0
afotiadis <afotiadis@ece.auth.gr>
2024-12-01 17:18:41 +0200

    Update ReservationService.js

--- service/ReservationService.js
+++ service/ReservationService.js
@@ -92,11 +92,9 @@ exports.makeReservation = function(body) {
       reject(error);
       return;
     }
-
     // Η κράτηση έχει περάσει όλους τους ελέγχους και μπορώ να την προσθέσω
     // Δεν το κάνω, καθώς η συνάρτηση είναι dummy
     resolve();
-  
   });
 }
 
@@ -117,32 +115,32 @@ exports.makeReservation = function(body) {
 exports.modifyReservation = function(body,spotId,userId,startTime,duration,date,id) {
   return new Promise(function(resolve, reject) {
  
-    // Η ημερομηνία της κράτησης πρεπει να είναι string (τύπος date)
-    if(typeof date !== 'string'){
-      const error = new Error('Invalid date.');
-      //Αν δεν είναι string επιστρέφει κωδικό αποτυχιας 400
-      error.response = {statusCode: 400};
-      reject(error);
-      return;
-    }
+    // // Η ημερομηνία της κράτησης πρεπει να είναι string (τύπος date)
+    // if(typeof date !== 'string'){
+    //   const error = new Error('Invalid date.');
+    //   //Αν δεν είναι string επιστρέφει κωδικό αποτυχιας 400
+    //   error.response = {statusCode: 400};
+    //   reject(error);
+    //   return;
+    // }
 
-    // Η ώρα λήξης (δηλ. στη περίπτωση μας η διάρκεια) της κράτησης πρεπει να είναι string (τύπος date-time)
-    if(typeof duration !== 'string'){
-      const error = new Error('Invalid duration.');
-      //Αν δεν είναι string επιστρέφει κωδικό αποτυχιας 400
-      error.response = {statusCode: 400};
-      reject(error);
-      return;
-    }
+    // // Η ώρα λήξης (δηλ. στη περίπτωση μας η διάρκεια) της κράτησης πρεπει να είναι string (τύπος date-time)
+    // if(typeof duration !== 'string'){
+    //   const error = new Error('Invalid duration.');
+    //   //Αν δεν είναι string επιστρέφει κωδικό αποτυχιας 400
+    //   error.response = {statusCode: 400};
+    //   reject(error);
+    //   return;
+    // }
 
-    // Η ώρα έναρξης της κράτησης πρεπει να είναι string (τύπος date-time)
-    if(typeof startTime !== 'string'){
-      const error = new Error('Invalid Start time.');
-      //Αν δεν είναι string επιστρέφει κωδικό αποτυχιας 400
-      error.response = {statusCode: 400};
-      reject(error);
-      return;
-    }
+    // // Η ώρα έναρξης της κράτησης πρεπει να είναι string (τύπος date-time)
+    // if(typeof startTime !== 'string'){
+    //   const error = new Error('Invalid Start time.');
+    //   //Αν δεν είναι string επιστρέφει κωδικό αποτυχιας 400
+    //   error.response = {statusCode: 400};
+    //   reject(error);
+    //   return;
+    // } ****Αφαιρέθηκαν από τον έλεγχο γιατί δεν υπάρχει ανάγκη, ο κωδικας του Swaggerhub καλυπτει αυτον τον ελεγχο****
 
     // το id της κράτησης πρέπει να είναι μη αρνητικός αριθμός
     if(!id || id < 0){
