b1576c7b79cf0d0fd7a6113de2df90f3138a181e
gpittis <giorgospittis3@gmail.com>
2024-12-20 11:05:01 +0200

    add comments

--- service/SpotOwnerService.js
+++ service/SpotOwnerService.js
@@ -14,46 +14,45 @@ exports.addSpotOwner = function(body) {
     if (!body.id || body.id < 0) {
       const error = new Error("Invalid id: must be a positive integer.");     // id must be non negative integer 
       error.response = { statusCode: 400 };    // if id is invalid then returns 400
-      reject(error);  
+      reject(error);   // reject the error
       return; 
-    }
+    };
 
     if (!body.idNumber || typeof body.idNumber !== "string") {
       const error = new Error("Invalid idNumber: must be a string.");   // id number must be string
       error.response = { statusCode: 400 };    // if idNumber is invalid then returns 400
-      reject(error);
+      reject(error);   // reject the error
       return; 
-    }
+    };
 
     if (!body.name || typeof body.name !== "string") {
       const error = new Error("Invalid name: must be a string.");   // name must be string
       error.response = { statusCode: 400 };   // if name is invalid then returns 400
-      reject(error);
+      reject(error);  // reject the error
       return; 
-    }
+    };
 
     if (!body.email || typeof body.email !== "string") {
       const error = new Error("Invalid email: must be a string.");   // email must be string
       error.response = { statusCode: 400 };    // if email is invalid then returns 400
-      reject(error);
+      reject(error);  // reject the error 
       return; 
-    }
+    };
     
     if (!body.phone) {
       const error = new Error("No phone.");  // phone must exists in SpotOwner's data
       error.response = { statusCode: 400 };  // if phone is non - existent then returns 400
-      reject(error);
+      reject(error);  // reject the error 
       return; 
-    }
+    };
 
     if (!body.spots) {
       const error = new Error("No spots.");   // Spots must exists in SpotOwner's data
       error.response = { statusCode: 400 };   // if the spot's list is non - existent then returns 400
-      reject(error);
+      reject(error);  // reject the error
       return; 
-    }
+    };
 
     resolve();
     });
-
 }
