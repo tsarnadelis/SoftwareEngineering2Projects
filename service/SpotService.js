f9466c6680732e7b2020b91fb871f77653ad824a
gpittis <giorgospittis3@gmail.com>
2024-12-20 10:20:52 +0200

    change comments

--- service/SpotService.js
+++ service/SpotService.js
@@ -19,36 +19,32 @@ exports.createSpot = function (body) {
         "chargerAvailability": false
       };
 
-    // Το address πρέπει να είναι string
+    // Το address πρέπει να είναι string. Αν το address έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
     if (!body.address) {
-      //αν το address έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
       const error = new Error("Invalid address: must be a string.");
       error.response = { statusCode: 400 };
       reject(error);
       return;
     }
     
-    //Το id πρέπει να είναι μη αρνητικός ακέραιος αριθμός
+    //Το id πρέπει να είναι μη αρνητικός ακέραιος αριθμός. Αν έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
     if (!body.id || body.id < 0) {
-      //αν το id έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
       const error = new Error("Invalid id: must be a positive integer.");
       error.response = { statusCode: 400 };
       reject(error);
       return;
     }
 
-    //Το type πρέπει να ισούται με "Garage" ή "Open" ή "Underground"
+    //Το type πρέπει να ισούται με "Garage" ή "Open" ή "Underground". Διαφορετικά, έχω σφάλμα με κωδικό σφάλματος 400
     if (!["Garage", "Open", "Underground"].includes(body.type)) {
-      //αν το type έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
       const error = new Error("Invalid type: must be one of 'Garage', 'Open', or 'Underground'.");
       error.response = { statusCode: 400 };
       reject(error);
       return;
     }
 
-    //Το chargerAvailability πρέπει να είναι boolean
+    //Το chargerAvailability πρέπει να είναι boolean. Διαφορετικά, έχω σφάλμα με κωδικό σφάλματος 400
     if (typeof body.chargerAvailability !== 'boolean') {
-      //αν το chargerAvailability έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
       const error = new Error("Invalid chargerAvailability: must be a boolean.");
       error.response = { statusCode: 400 };
       reject(error);
@@ -62,17 +58,14 @@ exports.createSpot = function (body) {
                existingSpots.address === body.address &&
                existingSpots.type === body.type &&
                existingSpots.chargerAvailability === body.chargerAvailability;
-    
+
+    //Αν βρεθεί διπλότυπη θέση τότε έχω σφάλμα με κωδικό σφάλματος 400           
     if (spotExists) {
-      //Αν βρεθεί διπλότυπη θέση τότε έχω σφάλμα με κωδικό σφάλματος 400
       const error = new Error("Spot already exists: a spot with the same attributes already exists.");
       error.response = { statusCode: 400 };
       reject(error);
       return;
     }
-
-    // Η θέση έχει περάσει όλους τους ελέγχους και μπορώ να την προσθέσω
-    // Δεν το κάνω, καθώς η συνάρτηση είναι dummy
 
     resolve();
   });
@@ -120,9 +113,7 @@ exports.getSpots = function () {
  * no response value expected for this operation
  **/
 exports.modifySpot = function (body, address, type, charger, id) {
-  return new Promise((resolve, reject) => { // Οι έλεγχοι που έγιναν comment εκτελούνται από το swagger, 
-    // επομένως είναι περιττό να τους γράψω μέσα στο modifySpot διότι μου χαλάνε το coverage του SpotService.js
-
+  return new Promise((resolve, reject) => {
     // Τα δεδομένα των ήδη καταχωρημένων θέσεων μέσα στο σύστημα
     var existingSpots = {
       address: "Navarinou 18",
@@ -131,24 +122,22 @@ exports.modifySpot = function (body, address, type, charger, id) {
       charger: false,
     };
 
-    //Το type πρέπει να ισούται με "Garage" ή "Open" ή "Underground"
+    //Το type πρέπει να ισούται με "Garage" ή "Open" ή "Underground". Διαφορετικά, έχω σφάλμα με κωδικό σφάλματος 400
     if (!["Garage", "Open", "Underground"].includes(type)) {
-      //αν το type έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
       const error = new Error("Invalid type in query: must be one of 'Garage', 'Open', or 'Underground'.");
       error.response = { statusCode: 400 };
       reject(error);
       return;
     }
 
-    //Το id πρέπει να είναι μη αρνητικός ακέραιος αριθμός
+    //Το id πρέπει να είναι μη αρνητικός ακέραιος αριθμός. Διαφορετικά, έχω σφάλμα με κωδικό σφάλματος 400
     if (!id || id < 0) {
-      //αν το id έχει μη έγκυρη τιμή τότε έχω σφάλμα με κωδικό σφάλματος 400
       const error = new Error("Invalid id in query: must be a positive integer.");
       error.response = { statusCode: 400 };
       reject(error);
       return;
     }
-    
+
     // Σύγκριση query παραμέτρων με request body attributes. 
     // Τα αντίστοιχα query attributes με τα αντίστοιχα attributes του request body πρέπει να ταυτίζονται. 
     if (body.address !== address) {
@@ -181,15 +170,14 @@ exports.modifySpot = function (body, address, type, charger, id) {
 
     // Έλεγχος αν τα τροποποιημένα στοιχεία της θέσης ταυτίζονται με τα "παλιά" στοιχεία της θέσης. 
     //Ουσιαστικά, δεν γίνεται τροποποίηση.
-    const spotExists =
     //Δεν ελέγχω τα id του existingSpots και του body διότι θεωρώ εκ των πραγμάτων ότι ισούται μεταξύ τους.
     // Αυτό συμβαίνει διότι κάνω modify μια ήδη υπάρχουσα θέση , άρα το id δεν αλλάζει κατά το modification.
     // Αν άλλαζε το id τότε θα έκανα create μια νέα θέση. 
     // Αντίστοιχος έλεγχος έγινε και στο PUT /licensePlate.
     //Επίσης αυτός ο έλεγχος αφορά και το PUT /reservation  , αλλά δεν γράφτηκε για λόγους συντομίας. 
-      existingSpots.address === body.address &&
-      existingSpots.type === body.type &&
-      existingSpots.charger === body.charger;
+    const spotExists = existingSpots.address === body.address && 
+    existingSpots.type === body.type &&
+    existingSpots.charger === body.charger;
 
     if (spotExists) {
       const error = new Error("Spot already exists: a spot with the same attributes already exists.");
@@ -197,9 +185,6 @@ exports.modifySpot = function (body, address, type, charger, id) {
       reject(error);
       return;
     }
-
-    // Η θέση έχει περάσει όλους τους ελέγχους και μπορώ να την τροποποιήσω
-    // Δεν το κάνω, καθώς η συνάρτηση είναι dummy
 
     resolve();
   });
